de6e4f8539e5686a2791e6b707ad712b075dd289
Sebastian Wentworth <110143758+wangyinyuan@users.noreply.github.com>
2024-07-05 10:17:40 +0800

    feat: 增加 token 验证和错误处理的中间件

---
+++ online_homework/express_be/routes/middleware/tokenHandler.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'your_jwt_secret';
+
+const tokenHandler = {
+  generateToken: (user) => {
+    return jwt.sign({ id: user._id, username: user.username }, secret, { expiresIn: '1h' });
+  },
+  verifyToken: (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) {
+      return res.status(403).send('A token is required for authentication');
+    }
+    try {
+      const decoded = jwt.verify(token, secret);
+      req.user = decoded;
+    } catch (err) {
+      return res.status(401).send('Invalid Token');
+    }
+    return next();
+  }
+};
+
+module.exports = tokenHandler;
